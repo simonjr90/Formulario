e05ce6613c17a625ee9c9d1a6cc16914511ae6a8
simonjr90 <32305630+simonjr90@users.noreply.github.com>
2018-03-05 19:29:24 +0100

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -14,7 +14,7 @@ window.onload = function(){
         questionHandler(this);
         }
     };
-    xhttp.open("GET", "https://cdn.rawgit.com/simonjr90/asd/197b20ea/xml/XML.xml", true); 
+    xhttp.open("GET", "https://cdn.rawgit.com/simonjr90/Formulario/35090b83/xml/XML.xml", true); 
     xhttp.send();
 }
 
